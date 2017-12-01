55e56b28dd963adb21b64ede87b9644bcd841fd9
Xian Yang <kurt.goedel.1930@googlemail.com>
2017-12-01 17:10:14 +0100

    change from asus

--- src/main/scala/SparkTPApp5Partitionnement.scala
+++ src/main/scala/SparkTPApp5Partitionnement.scala
@@ -1,4 +1,4 @@
-import java.lang.Math.{ceil, max, min, sqrt}
+import java.lang.Math.{ceil, sqrt}
 
 import SparkTPApp4Zone.{Zone, calculMaxMin}
 
@@ -18,8 +18,7 @@ object SparkTPApp5Partitionnement {
 
   val compte = "p1715490"
 
-/*  class Case(minRA: Double, maxRA: Double, minDecl: Double, maxDecl: Double, val nom: String)
-    extends Zone(minRA, maxRA, minDecl, maxDecl)*/
+
 
 
 
@@ -27,7 +26,7 @@ object SparkTPApp5Partitionnement {
     val nbCasesRacineCarre: Int =
       if (nbMinCases < 0 || nbMinCases > 26 * 26) {
         println("Try again!")
-        1 // A remplir par un message d'exception!
+        1 // A remplir par un message d'exception !
       } else
         ceil(sqrt(nbMinCases)).toInt // à améliorer avec ifelse
 
@@ -36,11 +35,6 @@ object SparkTPApp5Partitionnement {
 
     val grilleDecl: NumericRange[Double] = limites.minDecl to limites.maxDecl by
       (limites.maxDecl - limites.minDecl) / nbCasesRacineCarre
-
-/*    val CasesRetournees: Array[Case] =
-      for ((ra, i) <- grilleRA.zipWithIndex)
-        for ((decl, j) <- grilleDecl.zipWithIndex)
-          yield new Case()*/
   }
 
   def dansQuelleCaseVaisJe (ra: Double, decl: Double, grille: Grille): String = {
@@ -75,13 +69,12 @@ object SparkTPApp5Partitionnement {
       val conf = new SparkConf().setAppName("SparkTPApp5Partitionnement-" + compte)
       val sc = new SparkContext(conf)
 
-      val nbMinCases = args(1).toInt
-      //val limitesEnArray = args(2).split(",").map(_.trim.toDouble)
-      val inputDir = args(2)
-      val outputDir = args(3)
+      val nbMinCases = args(0).toInt
 
-      /*val maGrille = Grille(nbMinCases, Zone(limitesEnArray(0), limitesEnArray(1),
-        limitesEnArray(2), limitesEnArray(3)))*/
+      val inputDir = args(1)
+      val outputDir = args(2)
+
+
       val maGrille = Grille(nbMinCases, calculMaxMin(inputDir, sc))
       val RDDaEcrire = reformerEnPairRDD(inputDir, sc, maGrille)
 
@@ -93,9 +86,7 @@ object SparkTPApp5Partitionnement {
 
     else {
       println("Usage: spark-submit --class SparkTPApp5Partitionnement /home/" + compte + "/SparkTPApp-correction-assembly-1.0.jar " +
-        "nbMinCases" +
-        "MinMaxRaDeclSeparesParVirgule" +
-        "[ --caseClass | --tuple ] " +
+        "nbMinCases " +
         "hdfs:///user/" + compte + "/repertoire-donnees " + /* half useful for the moment */
         "hdfs:///user/" + compte + "/repertoire-resultat")
     }
---
+++ src/test/scala/SparkTPApp6ExtentionsTest1.scala
@@ -0,0 +1,35 @@
+/**
+  * Created by Xian on 2017/12/1.
+  */
+
+import SparkTPApp6Extensions.dansQuellesCasesVaisJe
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{BeforeAndAfter, FunSuite}
+class SparkTPApp6ExtentionsTest1 extends FunSuite with BeforeAndAfter  {
+  var sc: SparkContext = null;
+
+  // exécuter avant chaque test
+  before {
+    // configuration de Spark
+    val conf = new SparkConf()
+      .setAppName("SparkTPApp3Test")
+      .setMaster("local") // ici on configure un faux cluster spark local pour le test
+    sc = new SparkContext(conf)
+  }
+
+  test("Cases ok ?") {
+    dansQuellesCasesVaisJe(1, 3.0).foreach(println)
+    dansQuellesCasesVaisJe(359, 3.0).foreach(println)
+    dansQuellesCasesVaisJe(8, 3.0).foreach(println)
+    dansQuellesCasesVaisJe(17, 3.0).foreach(println)
+    dansQuellesCasesVaisJe(351, 3.0).foreach(println)
+    dansQuellesCasesVaisJe(10, 3.0).foreach(println)
+    dansQuellesCasesVaisJe(11, 3.0).foreach(println)
+  }
+
+  // exécuter après chaque test
+  after {
+    sc.stop() // a ne pas oublier, car il ne peut pas y avoir 2 contextes spark locaux simultanément
+  }
+
+}
