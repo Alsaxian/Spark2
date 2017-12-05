461494511640bffd8de1d0be9e68382ff156ad2a
YANG <xian.yang@etu.univ-lyon1.fr>
2017-12-05 02:27:50 +0100

    imports

--- src/main/scala/SparkTPApp7ExtNew.scala
+++ src/main/scala/SparkTPApp7ExtNew.scala
@@ -1,13 +1,12 @@
 import java.lang.Math._
 
 import TIW6RDDUtils.writePairRDDToHadoopUsingKeyAsFileName
-
-import scala.collection.mutable.{Buffer, ListBuffer}
+import scala.collection.immutable.List
+import scala.collection.mutable.ListBuffer
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import schema.PetaSkySchema
 
-import scala.collection.immutable.List
 
 
 /**
